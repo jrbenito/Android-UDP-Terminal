63e0b44b6ff59f7b4a42a6a9b08db89f0ca7b57b
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2011-09-20 08:35:37 +0900

    Removing duplicate packet send (left for test)\nLet user set local port

--- src/com/benito/udpterminal/TerminalUDPActivity.java
+++ src/com/benito/udpterminal/TerminalUDPActivity.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -16,16 +15,14 @@ import android.widget.Toast;
 
 public class TerminalUDPActivity extends Activity {
 	
-	private int sendPacket(String ipHost, int port, String payload) throws IOException {
+	private int sendPacket(int lPorta,String ipHost, int port, String payload) throws IOException {
 		
 		InetAddress address = InetAddress.getByName(ipHost);
 				
-		DatagramSocket socket = new DatagramSocket();
+		DatagramSocket socket = new DatagramSocket(lPorta);
 		
 		DatagramPacket packet = new DatagramPacket(payload.getBytes(),payload.length(),address,port);
 		
-		socket.send(packet);
-		socket.send(packet);
 		socket.send(packet);
 		socket.disconnect();
 		socket.close();
@@ -37,22 +34,13 @@ public class TerminalUDPActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        InetAddress localIp = null;
         setContentView(R.layout.main);
         
+        final EditText edLporta = (EditText) findViewById(R.id.editTextLocalPort);
         final EditText edIP = (EditText) findViewById(R.id.editTextIp);
         final EditText edPorta = (EditText) findViewById(R.id.editTextPorta);
         final EditText edPayload = (EditText) findViewById(R.id.editTextPayload);
         final Button btSend = (Button) findViewById(R.id.buttonSend);
-        
-        try {
-        	localIp = InetAddress.getLocalHost();
-		} catch (UnknownHostException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		//Toast.makeText(TerminalUDPActivity.this, "Local IP: "+localIp.toString(),Toast.LENGTH_SHORT).show();
         
         btSend.setOnClickListener(new OnClickListener() {
         	public void onClick(View v) {
@@ -62,8 +50,9 @@ public class TerminalUDPActivity extends Activity {
         		Toast.makeText(TerminalUDPActivity.this, "enviando:\n"+texto ,Toast.LENGTH_LONG).show();
         		
         		int port = Integer.parseInt(edPorta.getText().toString());
+        		int lport = Integer.parseInt(edLporta.getText().toString());
         		try {
-					sendPacket(edIP.getText().toString(), port, edPayload.getText().toString());
+					sendPacket(lport,edIP.getText().toString(), port, edPayload.getText().toString());
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
